aefffdaab0a46464f37209d50249757ce3280766
doctor11 <1372278190@qq.com>
2025-07-11 23:30:14 +0800

    20250711

--- Instant-ngp/code/nerf_run_new_30_relu copy.py
+++ Instant-ngp/code/nerf_run_new_30_relu copy.py
@@ -361,10 +361,10 @@ device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
 print(torch.cuda.is_available())
 
 # 载入数据
-folder_path = '/DATA/disk1/asteroid/asteroid_inverse/Instant-ngp/new_dataset/sys_data/geo_round/30du_40dB'
+folder_path = '/DATA/disk1/asteroid/asteroid_inverse/Instant-ngp/new_dataset/sys_data/geo_round/30du_5dB'
 
 # 生成保存路径
-experiment_name = 'experiment148'
+experiment_name = 'experiment154'
 if not os.path.exists('./Instant-ngp/model/'+ experiment_name):
     os.makedirs('./Instant-ngp/model/'+ experiment_name)
 
